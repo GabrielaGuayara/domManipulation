538a53b13cfe766641d29a2af12fcee75804ee00
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-20 12:09:33 -0400

    Created a variable named mainEl

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-import "./styles.css";
+const mainEl  = document.querySelector("main");
+let str = 'var(--main-bg)';
+mainEl.style.backgroundColor = str;
+mainEl.innerHTML = `<h1>DOM Manipultation</h1>`
+mainEl.classList.add('flex-ctr')
+
