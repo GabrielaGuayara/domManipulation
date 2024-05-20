2c3d6a0ab78728f13b45597e8c26c457065e49e6
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-20 12:17:39 -0400

    Added a class to topMenuEl

--- app.js
+++ app.js
@@ -8,3 +8,4 @@ mainEl.classList.add('flex-ctr')
 const topMenuEl = document.querySelector('#top-menu')
 topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
+topMenuEl.classList.add('flex-around')
