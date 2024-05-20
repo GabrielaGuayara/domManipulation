8b605cbb0e3b0a2f168d8853f33f8a8954cacacc
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-20 12:10:30 -0400

    Assigned var() to a string variable

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const mainEl  = document.querySelector("main");
 let str = 'var(--main-bg)';
 mainEl.style.backgroundColor = str;
-mainEl.innerHTML = `<h1>DOM Manipultation</h1>`
-mainEl.classList.add('flex-ctr')
+
 
