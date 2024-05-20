3f5843d038ed81db00119442915950544238d267
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-20 12:13:08 -0400

    Created a variable named topMenuEl

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ let str = 'var(--main-bg)';
 mainEl.style.backgroundColor = str;
 mainEl.innerHTML = `<h1>DOM Manipultation</h1>`
 mainEl.classList.add('flex-ctr')
+
+
+const topMenuEl = document.querySelector('#top-menu')
