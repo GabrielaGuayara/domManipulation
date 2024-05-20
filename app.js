9469de0e0f2d9a8b90b95815f2a19e5f5fb7753e
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-20 12:11:33 -0400

    Added a class to mainEl

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const mainEl  = document.querySelector("main");
 let str = 'var(--main-bg)';
 mainEl.style.backgroundColor = str;
 mainEl.innerHTML = `<h1>DOM Manipultation</h1>`
-
+mainEl.classList.add('flex-ctr')
