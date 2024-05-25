fb20a358c88688fa08e1ccd5b11e0ddc7af1ec8b
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-24 20:53:11 -0400

    Adding comments to explain hwo my code works

--- app.js
+++ app.js
@@ -16,128 +16,130 @@ var menuLinks = [
   ]},
 ];
 
-//Creating a variable that allow  to change the background color, have text content and add a class
+//Creating a variable to select the main element from HTML doc and change the background color, have text content and add a class
 const mainEl  = document.querySelector("main");
 let str = 'var(--main-bg)';
 mainEl.style.backgroundColor = str;
 mainEl.innerHTML = `<h1>DOM Manipultation</h1>`
 mainEl.classList.add('flex-ctr')
 
-//Creating the variable topMenuEl using querySelector that allows to change the height, change the background and add a classList to the top of the nav bar'
+//Creating the variable to select the top menu from HTML doc and change the height, change the background and add a classList to the top of the nav bar'
 const topMenuEl = document.querySelector('#top-menu')
 topMenuEl.style.height = '100%';
 topMenuEl.style.backgroundColor = 'var(--top-menu-bg)'
 topMenuEl.classList.add('flex-around')
 
 
-//
-menuLinks.forEach(link =>{
+//For Each loop to iterate the menu link array and create the top menu links dinamically
+menuLinks.forEach(item =>{
   let aElem = document.createElement("a")
-  aElem.setAttribute("href", link.href)
-  aElem.textContent = link.text
+  aElem.setAttribute("href", item.href)
+  aElem.textContent = item.text
   topMenuEl.append(aElem)
 });
 
-
-for(let i=0; i<menuLinks.length; i++) {
+//I used for loop to iterate the menu arrays because I did not have clear understandin about forEach. After reading, I understand how it works and I feel more confident using it so I updated the code.
+// for(let i=0; i<menuLinks.length; i++) {
         
-    let link = document.createElement("a")
-    link.setAttribute("href", `${menuLinks[i].href}`)
-    link.innerHTML = `${menuLinks[i].text}`
-    topMenuEl.append(link)
-};
+//     let link = document.createElement("a")
+//     link.setAttribute("href", `${menuLinks[i].href}`)
+//     link.innerHTML = `${menuLinks[i].text}`
+//     topMenuEl.append(link)
+// };
 
 
 
 
 //DOM MANIPULATION: PART TWO BEGINS HERE
 
+//Getting the sub menu element from HTML doc using query selector.
 const subMenuEl = document.querySelector("#sub-menu");
 
+//Changing height and class and adding a new class to sub menu
 subMenuEl.style.height = '100%'
 let str2 = 'var(--sub-menu-bg)';
 subMenuEl.style.backgroundColor = str2;
 subMenuEl.classList.add('flex-around')
 
 
+//Getting the links within the top menu nav bar
 const topMenuLinks = document.querySelector("#top-menu a")
 // console.log(topMenuLinks)
 
 
 
+//Using event listener to handle click events on the elements within the top menu element
 topMenuEl.addEventListener("click", (e)=>{
+
   // console.log(e.target)
   e.preventDefault();
   // console.log(e)
   if(!e.target.matches("a")){
       return;
   }
+
+  //Utilized array.from to convert a nodelist into an array 
+  const arrOfTopLinks = Array.from(topMenuLinks);
+    
   
-  // console.log(e.target)
-  
+  //This forEach will remove the class active from links previously clicked
+  arrOfTopLinks.forEach(aElem => {
+    if(aElem != e.target){
+    aElem.classList.remove("active")}
+  });
+
+
+  //Add a active class to the clicked links
   e.target.classList.add("active");
   
-  const arrOfLinks = Array.from(topMenuLinks);
-  // console.log(arrOfLinks)
-
-  
-  arrOfLinks.forEach(aElem=>{
-    
-    // console.log(aElem.target)
-  
-    if(aElem !== e.target){
-      aElem.classList.remove("active")
-    }
-  });
-
  
   // console.log(e.target.textContent)
-  if(e.target.textContent !== 'About'){
+  if(e.target.textContent.toLowerCase() !== 'about'){
+    //If the target content does not match the word about. The function buildSubmenu would be called and pass as parameters those sublinks of the top menu links that match with the content the target element
     buildSubmenu(menuLinks.find(item => item.text === e.target.textContent).subLinks)
     subMenuEl.style.top = '100%';
+    } else{
+    subMenuEl.style.top = '0%';}
 
-}
-  else{
-    subMenuEl.style.top = '0';
+  //If the about link is clicked show about
+  if(e.target.textContent === "about"){
+ 
+    mainEl.innerHTML= `<h1>About</h1>`
   }
 
-  });
 
+});
+
+
+/*This is a helper function takes as paremeters the sublinks. Then clean the subMenulEl content and use a ForEach loop to iterate over the sublinks and create links asign them 
+attributes and set their content. Finally, adds the elements to the sub menu into the HTML doc
+*/
 function buildSubmenu(subLinks){
     subMenuEl.innerHTML = "";
-    subLinks.forEach(link=>{
+    subLinks.forEach(sublink=>{
       let aElem = document.createElement("a");
-      aElem.setAttribute("href", link.href)
-      aElem.textContent = link.text;
+      aElem.setAttribute("href", sublink.href)
+      aElem.textContent = sublink.text;
       subMenuEl.append(aElem)
     })
 
 }
 
+
+//Using event listener to handle click events on elements within the sub menu in the HTML doc
 subMenuEl.addEventListener("click", (e)=>{
   e.preventDefault();
+  
+  //console.log(e.target)
   if(!e.target.matches('a')){
     return;
   }
   
-  //console.log(e.target)
-  
   subMenuEl.style.top = '0';
-  
-  topMenuLinks.forEach(aElem =>{ aElem.classList.remove('active')});
 
-  
+  //Adding the header for the mainEl with the clicked <a> element
   mainEl.innerHTML=`<h1>${e.target.textContent}</h1>`
 
-
-
-  // console.log(subLinks)
-
-
-  if(e.target.textContent === "about"){
- 
-    mainEl.innerHTML= `<h1>About</h1>`
-  }
 
 });
 
